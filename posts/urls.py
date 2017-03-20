ad7f0328c2f1647e2b503989d2b614bb550ebfde
mugabiisaac <imugabi1@gmail.com>
2017-03-20 18:23:30 +0300

    Add existing file

--- posts/urls.py
+++ posts/urls.py
@@ -16,7 +16,7 @@ from .views import (
 urlpatterns = [
     url(r'^$', post_list, name='list'),
     #url(r'^posts/', include('posts.urls')),
-    url(r'^create/$', post_create),
+    url(r'^create/$', post_create, name='create'),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
     url(r'^(?P<id>\d+)/delete/$', post_delete),
