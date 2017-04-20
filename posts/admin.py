9c6ff46b1828f55c82099cbb3f36e2649c6814b5
mugabiisaac <imugabi1@gmail.com>
2017-04-21 00:03:20 +0300

    Tesh heroku progress commit

--- posts/admin.py
+++ posts/admin.py
@@ -15,9 +15,19 @@ class PostModelAdmin(admin.ModelAdmin):
     list_filter = ["updated", "timestamp"]
 
     search_fields = ["title", "content"]
+    ordering = ['title']
+    actions = [booked]
 
     class Meta:
         model = Post
+
+    def booked(self, request, queryset):
+        rows_updated = queryset.update(status='b')
+        if rows_updated ==1:
+            message_bit = "1 item was"
+        else:
+            message_bit = "%s items were" % rows_updated
+        self.message_user(request, "%s successfully marked as booked." % message_bit)
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ["user", "location", "bio", "birth_date", "company", "url"]
