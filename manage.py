679556aede427568ff3052124b8e9f6e0a9e4881
mugabiisaac <imugabi1@gmail.com>
2017-05-01 11:07:43 +0300

    Added a Procfile

--- manage.py
+++ manage.py
@@ -4,8 +4,10 @@ import sys
 
 
 
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kaka2.settings")
+    django.setup()
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
